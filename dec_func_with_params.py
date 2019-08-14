a441e730da2ed0db910f5625a3e487e44a37a37b
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-08-14 11:02:28 +0300

    Update dec_func_with_params.py

--- dec_func_with_params.py
+++ dec_func_with_params.py
@@ -1,3 +1,5 @@
+# Example1
+
 def easy_decorator(f):
     def improved_f(*args, **kwags):
         if kwags.get("pip"):
@@ -19,3 +21,37 @@ def do_sum(*args, **kwargs):
 
 
 print(do_sum(1, 2, 3, 4, 5, pip=1))
+
+# Example2
+
+from operator import mul
+from functools import reduce
+
+
+def decorate_function(f):
+    def decorator(*args, **kwargs):
+        print("Hello")
+        if "_k" in kwargs.keys():
+            new_args = []
+            for i in args:
+                i += 1
+                new_args.append(i)
+            print(new_args)
+            result = f(new_args, **kwargs)
+        else:
+            new_args = []
+            for i in args:
+                i -= 1
+                new_args.append(i)
+            print(new_args)
+            result = f(new_args, **kwargs)
+        return result
+    return decorator
+
+
+@decorate_function
+def g(*args, **kwargs):
+    if "_k" in kwargs.keys():
+        return sum(*args)
+    else:
+        return reduce(mul, *args)
