858f872b7d3b299f8b1239fc27b8d74ed10b8917
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-02-19 17:08:04 +0300

    Create async_set_task_factory.py

---
+++ async_set_task_factory.py
@@ -0,0 +1,30 @@
+import asyncio
+from random import randint
+
+
+def my_factory(loop, coro):
+    f = asyncio.Task(coro, loop=loop)
+    f.name = f"TaskNameIs {randint(100, 999)}"
+    return f
+
+
+async def pook():
+    for i in range(5):
+        print(f"Step-{i}")
+        await asyncio.sleep(1)
+
+
+async def main():
+    x = asyncio.create_task(pook())
+    y = asyncio.create_task(pook())
+    await x
+    await y
+    print(x.name)
+    print(y.name)
+
+if __name__ == '__main__':
+    _loop = asyncio.get_event_loop()
+    _loop.set_debug(True)
+    _loop.set_task_factory(my_factory)
+    _loop.run_until_complete(main())
+    _loop.close()
